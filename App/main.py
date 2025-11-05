57361e10b8d35481a6ebabb2ec0e967ee8c68240
ahmad777x86 <087m.ahmad@gmail.com>
2025-11-06 00:42:13 +0500

    Made basic API endpoints

--- App/main.py
+++ App/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from utils import load_model, preprocess_image
+from fastapi import UploadFile, File
 
-
+model = None
 
 @asynccontextmanager
 def lifespan(app: FastAPI):
-    load_model()
-
+    global model
+    model = load_model()
 
 app = FastAPI(lifespan=lifespan)
+
+@app.get('/')
+def health_check():
+    return "Health Check: Success"
+
+@app.post('/predict')
+async def predict(file: UploadFile = File(...)):
+    image_data = await file.read()
+    image = preprocess_image(image_data)
+    prediction = model(file)
+    prediction = prediction.argmax(dim=1).item()
+    return {"Prediction" : prediction}
--- App/utils.py
+++ App/utils.py
@@ -1,3 +1,13 @@
+import torch
+import numpy as np
+import cv2
 
+def load_model(path : str = "../Model/model.pth"):
+    model = torch.load(path, weights_only=False)
+    print("Model Successfully Loaded")
+    return model
 
-def load_model(path : str = "..Model\model.pth"):
+def preprocess_image(image_data):
+    array = np.frombuffer(image_data,dtype=np.uint8)
+    image = cv2.imdecode(array, cv2.IMREAD_GRAYSCALE)
+    print("Image has successfully been decoded")
